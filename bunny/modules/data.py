d8316b1fc4dac323f68a61bd22d584d45db72e49
MrRaBBiT <darkworldxd09@gmail.com>
2023-11-13 20:09:18 +0530

    Create data.py

---
+++ bunny/modules/data.py
@@ -0,0 +1,40 @@
+from bunny import mongodb
+from typing import Dict, List, Union
+
+
+
+permitdb = mongodb.pmprotection
+
+async def is_approved() -> list:
+    pm = await permitdb.find_one({'permit': 'protection'})
+    if not pm:
+        return []
+    return pm['users']
+
+
+async def approve(user_ud: int):
+    pm = await is_approved()
+    pm.append(user_ud)
+    await permitdb.update_one(
+        {'permit': 'protection'},
+        {
+            '$set': {
+                'users': pm
+            }
+        },
+        upsert=True
+    )
+
+
+async def disapprove(user_ud: int):
+    pm = await is_approved()
+    pm.remove(user_ud)
+    await permitdb.update_one(
+        {'permit': 'protection'},
+        {
+            '$set': {
+                'users': pm
+            }
+        },
+        upsert=True
+    )
