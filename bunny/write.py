128584c31ce564f63e07feb491345af2fc563250
Geek <74235427+Geektyper@users.noreply.github.com>
2023-11-21 15:31:35 +0530

    Update write.py

--- bunny/write.py
+++ bunny/write.py
@@ -1,23 +1,71 @@
-from pyrogram import filters
+import datetime
 
-from config import STUFF , DEV
-from bunny import neko
+from pyrogram import Client, filters, types
 
-HANDLER = STUFF.COMMAND_HANDLER
+from bunny.utils.misc import modules_help, prefix
+from bunny.utils.db import db
 
-OWNER_ID = DEV.OWNER_ID
 
-@neko.on_message(filters.command("write", prefixes=HANDLER) & filters.user(OWNER_ID))
-async def write(_, message):
-    if len(message.command) < 2:
-        return await message.reply_text("ᴘʟᴢ ɢɪᴠᴇ ᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴡʀɪᴛᴇ ғɪʀsᴛ ")
-    m = await message.reply_text("ᴡʀɪᴛɪɴɢ...")
-    name = (
-        message.text.split(None, 1)[1]
-        if len(message.command) < 3
-        else message.text.split(None, 1)[1].replace(" ", "%20")
+# avoid using global variables
+afk_info = db.get(
+    "core.afk",
+    "afk_info",
+    {
+        "start": 0,
+        "is_afk": False,
+        "reason": "",
+    },
+)
+
+is_afk = filters.create(lambda _, __, ___: afk_info["is_afk"])
+is_support = filters.create(lambda _, __, message: message.chat.is_support)
+
+
+@Client.on_message(
+    is_afk
+    & (filters.private | filters.mentioned)
+    & ~filters.channel
+    & ~filters.me
+    & ~filters.bot
+    & ~is_support
+)
+async def afk_handler(_, message: types.Message):
+    start = datetime.datetime.fromtimestamp(afk_info["start"])
+    end = datetime.datetime.now().replace(microsecond=0)
+    afk_time = end - start
+    await message.reply(
+        f"<b>I'm AFK {afk_time}\nReason:</b> <i>{afk_info['reason']}</i>"
     )
-    hand = "https://apis.xditya.me/write?text=" + name
-    await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ...")
-    await m.delete()
-    await message.reply_photo(hand, caption="**Meet Me Here🙈 @notrealgeek**")
+
+
+@Client.on_message(filters.command("afk", prefix) & filters.me)
+async def afk(_, message):
+    if len(message.text.split()) >= 2:
+        reason = message.text.split(" ", maxsplit=1)[1]
+    else:
+        reason = "None"
+
+    afk_info["start"] = int(datetime.datetime.now().timestamp())
+    afk_info["is_afk"] = True
+    afk_info["reason"] = reason
+
+    await message.edit(f"<b>I'm going AFK.\n" f"Reason:</b> <i>{reason}</i>")
+
+    db.set("core.afk", "afk_info", afk_info)
+
+
+@Client.on_message(filters.command("unafk", prefix) & filters.me)
+async def unafk(_, message):
+    if afk_info["is_afk"]:
+        start = datetime.datetime.fromtimestamp(afk_info["start"])
+        end = datetime.datetime.now().replace(microsecond=0)
+        afk_time = end - start
+        await message.edit(
+            f"<b>I'm not AFK anymore.\n" f"I was afk {afk_time}</b>"
+        )
+        afk_info["is_afk"] = False
+    else:
+        await message.edit("<b>You weren't afk</b>")
+
+    db.set("core.afk", "afk_info", afk_info)
+
