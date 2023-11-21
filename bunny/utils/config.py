75a22c3764563ceea1ddb583df3ddee92a20fae5
Geek <74235427+Geektyper@users.noreply.github.com>
2023-11-21 16:17:41 +0530

    Update config.py

--- bunny/utils/config.py
+++ bunny/utils/config.py
@@ -6,8 +6,8 @@ env.read_env("./.env")
 api_id = env.int("API_ID")
 api_hash = env.str("API_HASH")
 
-db_type = env.str("DATABASE_TYPE")
-db_url = env.str("DATABASE_URL", "")
+db_type = env.str("DATABASE_TYPE", "Mongo")
+db_url = env.str("DATABASE_URL", "true")
 db_name = env.str("DATABASE_NAME")
 
 test_server = env.bool("TEST_SERVER", False
