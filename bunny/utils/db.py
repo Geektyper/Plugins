e19a9532de04ca067c24d18d77f075a0b5aadc3e
Geek <74235427+Geektyper@users.noreply.github.com>
2023-11-21 16:10:38 +0530

    Update db.py

--- bunny/utils/db.py
+++ bunny/utils/db.py
@@ -151,4 +151,4 @@ class SqliteDatabase(Database):
 if config.db_type in ["mongo", "mongodb"]:
     db = MongoDatabase(config.db_url, config.db_name)
 else:
-    db = SqliteDatabase(config.db_name
+    db = SqliteDatabase(config.db_name)
