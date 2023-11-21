1a99f024f73a2c223f78cdee63ee1f583914d398
Geek <74235427+Geektyper@users.noreply.github.com>
2023-11-21 15:44:55 +0530

    Update misc.py

--- bunny/utils/misc.py
+++ bunny/utils/misc.py
@@ -2,6 +2,8 @@ import os
 import sys
 from re import sub
 from time import time
+from sys import version_info
+from bunny.Databse.db import db
 
 from pyrogram import Client, enums
 
@@ -113,3 +115,28 @@ async def extract_args(message, markdown=True):
 
 async def extract_args_arr(message, markdown=True):
     return extract_args(message, markdown).split()
+
+
+
+class ModulesHelpDict(dict):
+    def append(self, obj: dict):
+        # convert help from old to new type
+        module_name = list(obj.keys())[0]
+        cmds = obj[module_name]
+        commands = {}
+        for cmd in cmds:
+            cmd_name = list(cmd.keys())[0]
+            cmd_desc = cmd[cmd_name]
+            commands[cmd_name] = cmd_desc
+        self[module_name] = commands
+
+
+modules_help = ModulesHelpDict()
+requirements_list = []
+
+python_version = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
+userbot_version = "1.5.0"
+
+prefix = db.get("core.main", "prefix", ".")
+
+
