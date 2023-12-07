3acd7250ebe7a55671b1475253449f874219ee90
Pratham <74235427+Geektyper@users.noreply.github.com>
2023-12-07 12:57:57 +0530

    Update plugins.py

--- bunny/plugins.py
+++ bunny/plugins.py
@@ -4,7 +4,6 @@ import time
 from .data import KeshavX
 from external_client import BOT
 from pyrogram import Client, filters
-from client import BOT
 
 hl = STUFF.COMMAND_HANDLER
 from .verify import verify
